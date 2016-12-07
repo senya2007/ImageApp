2178af369bdad190e4b85102d97b94898bdae919
senya2007 <penvik19@gmail.com>
2016-12-07 22:13:44 +0400

    Create standart structure

---
+++ ImageApp/Interfaces/IObject.cs
@@ -0,0 +1,15 @@
+﻿using ImageApp.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageApp.Interfaces
+{
+    public interface IObject
+    {
+        ObjectType Type { get; }
+        List<IObject> Children { get; set; }
+    }
+}
---
+++ ImageApp/Model/Row.cs
@@ -0,0 +1,33 @@
+﻿using ImageApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageApp.Helper;
+
+namespace ImageApp.Model
+{
+    public class Row : IObject
+    {
+        public List<IObject> Children
+        {
+            get
+            {
+                return Children;
+            }
+
+            set
+            {
+                Children = new List<IObject>(value);
+            }
+        }
+        public ObjectType Type
+        {
+            get
+            {
+                return ObjectType.Row;
+            }
+        }
+    }
+}
